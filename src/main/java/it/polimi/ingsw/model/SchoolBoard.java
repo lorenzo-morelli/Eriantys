3176b4326e100fb6c67a97fac59995820da9a8fd
Fernando Morea <91631198+fmorea@users.noreply.github.com>
2022-03-25 14:36:09 +0100

    Getter and setter fixes, part two

--- src/main/java/it/polimi/ingsw/model/SchoolBoard.java
+++ src/main/java/it/polimi/ingsw/model/SchoolBoard.java
@@ -16,30 +16,6 @@ public class SchoolBoard {
         this.towerQuantity = 0;
     }
 
-    public StudentSet getEntranceSpace() {
-        return entranceSpace;
-    }
-
-    public void setEntranceSpace(StudentSet entranceSpace) {
-        this.entranceSpace = entranceSpace;
-    }
-
-    public StudentSet getDinnerTable() {
-        return dinnerTable;
-    }
-
-    public void setDinnerTable(StudentSet dinnerTable) {
-        this.dinnerTable = dinnerTable;
-    }
-
-    public ProfessorBoard getProfessorSpace() {
-        return professorSpace;
-    }
-
-    public void setProfessorSpace(ProfessorBoard professorSpace) {
-        this.professorSpace = professorSpace;
-    }
-
     public Color getTowerColor() {
         return TowerColor;
     }
@@ -56,4 +32,59 @@ public class SchoolBoard {
         this.towerQuantity = towerQuantity;
     }
 
+
+    public int getEntranceSpacePopulation(Color color){
+        return get(color, this.entranceSpace);
+    }
+
+    public void setDinnerTablePopulation(int n, Color color){
+        set(n,color,this.dinnerTable);
+    }
+
+    public int getDinnerTablePopulation(Color color){
+        return get(color, this.dinnerTable);
+    }
+
+    public void setEntranceSpacePopulation(int n, Color color){
+        set(n,color,this.entranceSpace);
+    }
+
+    private int get(Color color, StudentSet object) {
+        if (color == Color.green) {
+            return object.getGreenColorSet().getCardinality();
+        }
+        if (color == Color.yellow) {
+            return object.getYellowColorSet().getCardinality();
+        }
+        if (color == Color.blue) {
+            return object.getBlueColorSet().getCardinality();
+        }
+        if (color == Color.pink) {
+            return object.getPinkColorSet().getCardinality();
+        }
+        if (color == Color.red) {
+            return object.getRedColorSet().getCardinality();
+        }
+        //ToDo: da gestire con una eccezione
+        return -1;
+    }
+
+
+    private void set(int n, Color color, StudentSet object) {
+        if (color == Color.green) {
+            object.getGreenColorSet().setCardinality(n);
+        }
+        if (color == Color.yellow) {
+            object.getYellowColorSet().setCardinality(n);
+        }
+        if (color == Color.blue) {
+            object.getBlueColorSet().setCardinality(n);
+        }
+        if (color == Color.pink) {
+            object.getPinkColorSet().setCardinality(n);
+        }
+        if (color == Color.red) {
+            object.getRedColorSet().setCardinality(n);
+        }
+    }
 }
