3daa457edb2d95e62438ff04202897c7ba59c412
lorenzo-morelli <lorenzo.morelli.bs@gmail.com>
2022-04-05 20:13:12 +0100

    added two controllers

---
+++ src/main/java/it/polimi/ingsw/client/controller_client/ControllerClient.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.client.controller_client;
+
+public class ControllerClient {
+}
---
+++ src/main/java/it/polimi/ingsw/server/controller_server/ControllerServer.java
@@ -0,0 +1,4 @@
+package it.polimi.ingsw.server.controller_server;
+
+public class ControllerServer {
+}
