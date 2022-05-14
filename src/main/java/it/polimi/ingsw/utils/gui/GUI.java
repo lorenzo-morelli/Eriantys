dedf7ee31ad91ecb54938426df5f0734af6d4264
lorenzo-morelli <lorenzo.morelli.bs@gmail.com>
2022-05-14 13:44:07 +0100

    gui created for main menu and setup

--- src/main/java/it/polimi/ingsw/utils/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/utils/gui/GUI.java
@@ -2,21 +2,42 @@ package it.polimi.ingsw.utils.gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class GUI extends Application {
 
+    Stage stage;
+    Scene scene;
+
+
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("src/main/resources/fxml/MainMenu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Eriantys");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) throws IOException {
+        this.stage = stage;
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/MainMenu.fxml"));
+            this.scene = new Scene(loader.load());
+            this.stage.setScene(scene);
+            this.stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void changeScene(String newScene, MouseEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/" + newScene + ".fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        this.scene = new Scene(loader.load());
+        this.stage.setScene(scene);
+        this.stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
--- src/main/java/it/polimi/ingsw/utils/gui/controllers/MainMenuController.java
+++ src/main/java/it/polimi/ingsw/utils/gui/controllers/MainMenuController.java
@@ -1,5 +1,57 @@
 package it.polimi.ingsw.utils.gui.controllers;
 
-public class MainMenuController {
+import it.polimi.ingsw.utils.gui.GUI;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainMenuController {
+    private final GUI gui = new GUI();
+
+    @FXML
+    private TextField nickname;
+    @FXML
+    private TextField ip;
+    @FXML
+    private TextField port;
+    @FXML
+    private Label notice;
+
+    public void play(MouseEvent mouseEvent) throws IOException {
+        this.gui.changeScene("SetupGame", mouseEvent);
+    }
+
+    public void exit() {
+        System.exit(0);
+    }
+
+    public void start() {
+        String nickname = this.nickname.getText();
+        String ipText = this.ip.getText();
+        String portText = this.port.getText();
+
+        if (nickname.equals("") || ipText.equals("") || portText.equals("")) {
+            this.notice.setText("FAILURE: missing parameters!");
+        } else if (this.nickname.getText().length() > 13) {
+            this.notice.setText("FAILURE: nickname must be less than 13 characters!");
+        } else if (ipText.contains(" ") || portText.contains(" ")) {
+            this.notice.setText("FAILURE: ip and port can't contain any spaces!");
+        } else {
+            this.notice.setText("Ok, per ora");
+            int ip = Integer.parseInt(ipText);
+            int port = Integer.parseInt(portText);
+            //TODO: connection verification and game connection with socket
+        }
+    }
 }
