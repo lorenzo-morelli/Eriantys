a0d070f608c4179bb5ca069b59909c06847c533d
lorenzo-morelli <lorenzo.morelli.bs@gmail.com>
2022-05-14 10:21:11 +0100

    MotherPhase created

--- src/main/java/it/polimi/ingsw/utils/gui/GUI.java
+++ src/main/java/it/polimi/ingsw/utils/gui/GUI.java
@@ -1,5 +1,22 @@
 package it.polimi.ingsw.utils.gui;
 
-public class GUI {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class GUI extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(GUI.class.getResource("src/main/resources/fxml/MainMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Eriantys");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
