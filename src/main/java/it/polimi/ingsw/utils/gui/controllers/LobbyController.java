0849226010269d021ec07b57a0a3cdb0260939f1
lorenzo-morelli <lorenzo.morelli.bs@gmail.com>
2022-05-17 22:36:49 +0100

    prepared LobbyController

--- src/main/java/it/polimi/ingsw/utils/gui/controllers/LobbyController.java
+++ src/main/java/it/polimi/ingsw/utils/gui/controllers/LobbyController.java
@@ -12,14 +12,15 @@ import java.util.ResourceBundle;
 public class LobbyController implements Initializable {
     private final GUI gui = new GUI();
     private final Gson gson = new Gson();
-    private int connectedPlayers;
 
     @FXML
     private Label otherPlayersLabel = new Label();
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        int connectedPlayers = 2;
         //todo: far arrivare numofplayer dal server + aggiornare connectedplayers
         this.otherPlayersLabel.setText("Waiting for other players... " + connectedPlayers + "/" + this.gui.getClientModel().getNumofplayer());
+        //todo:
     }
 }
