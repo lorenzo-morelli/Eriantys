cb89961ba14f21cbe2e663801aed76346a53ae6d
lorenzo-morelli <lorenzo.morelli.bs@gmail.com>
2022-05-24 12:50:55 +0200

    refactor

---
+++ src/main/java/it/polimi/ingsw/utils/common/SetConnection.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.utils.common;
+
+import it.polimi.ingsw.client.model.ClientModel;
+import it.polimi.ingsw.utils.network.Network;
+
+public class SetConnection {
+    public static void setConnection(String nickname, String ip, String port, ClientModel clientModel) {
+        clientModel.setNickname(nickname);
+        clientModel.setIp(ip);
+        clientModel.setPort(port);
+        Network.setupClient(ip, port);
+        clientModel.setMyIp(Network.getMyIp());
+    }
+}
