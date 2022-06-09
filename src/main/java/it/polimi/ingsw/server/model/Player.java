4d3156a802b22f32cf4fbb1a18f1c8567b860a4b
fmorea <fernando8morea@gmail.com>
2022-06-09 14:14:58 +0200

    Spelling fix

--- src/main/java/it/polimi/ingsw/server/model/Player.java
+++ src/main/java/it/polimi/ingsw/server/model/Player.java
@@ -96,19 +96,19 @@ public class Player implements Comparable<Player>{
                         (choosedCard == null ? "    CARD CHOOSED : NESSUNA\n" : "    CARD CHOOSED - VALUE : " + choosedCard.getValues() + " - MOVES : "+choosedCard.getMoves()+ "\n") +
                         "    SCHOOL\n"
                         + schoolBoard.toString() +
-                        (coins >= 0 ? "    COINS : " + coins + "\n" : (Objects.equals(nickname, this.nickname) ?"": "\n"))+(Objects.equals(nickname, this.nickname) ?"    AVAIABLE CARDS: "+ availableCards.toString()+"\n": "");
+                        (coins >= 0 ? "    COINS : " + coins + "\n" : (Objects.equals(nickname, this.nickname) ?"": "\n"))+(Objects.equals(nickname, this.nickname) ?"    AVAILABLE CARDS: "+ availableCards.toString()+"\n": "");
             case WHITE:
                 return ANSI_WHITE + "    PLAYER : " + this.nickname + ANSI_RESET + (isDisconnected ? "    IL GIOCATORE E' DISCONNESSO\n":"\n") +
                         (choosedCard == null ? "    CARD CHOOSED : NESSUNA\n" : "    CARD CHOOSED - VALUE : " + choosedCard.getValues() + " - MOVES : "+choosedCard.getMoves()+ "\n") +
                         "    SCHOOL\n"
                         + schoolBoard.toString() +
-                        (coins >= 0 ? "    COINS : " + coins + "\n" : (Objects.equals(nickname, this.nickname) ?"": "\n"))+(Objects.equals(nickname, this.nickname) ?"    AVAIABLE CARDS: "+ availableCards.toString()+"\n": "");
+                        (coins >= 0 ? "    COINS : " + coins + "\n" : (Objects.equals(nickname, this.nickname) ?"": "\n"))+(Objects.equals(nickname, this.nickname) ?"    AVAILABLE CARDS: "+ availableCards.toString()+"\n": "");
             case GREY:
                 return ANSI_GRAY + "    PLAYER : " + this.nickname + ANSI_RESET + (isDisconnected ? "    IL GIOCATORE E' DISCONNESSO\n":"\n")+
                         (choosedCard == null ? "    CARD CHOOSED : NESSUNA\n" : "    CARD CHOOSED - VALUE : " + choosedCard.getValues() + " - MOVES : "+choosedCard.getMoves()+ "\n") +
                         "    SCHOOL\n"
                         + schoolBoard.toString() +
-                        (coins >= 0 ? "    COINS : " + coins + "\n" : (Objects.equals(nickname, this.nickname) ?"": "\n"))+(Objects.equals(nickname, this.nickname) ?"    AVAIABLE CARDS: "+ availableCards.toString()+"\n": "");
+                        (coins >= 0 ? "    COINS : " + coins + "\n" : (Objects.equals(nickname, this.nickname) ?"": "\n"))+(Objects.equals(nickname, this.nickname) ?"    AVAILABLE CARDS: "+ availableCards.toString()+"\n": "");
         }
         return null;
     }
