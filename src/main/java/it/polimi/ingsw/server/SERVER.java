b0c9d03d9937cfe1c06191a134b9cdf5b131e103
lorenzo-morelli <lorenzo.morelli.bs@gmail.com>
2022-06-18 13:48:57 +0200

    added documentation for gui

--- src/main/java/it/polimi/ingsw/server/SERVER.java
+++ src/main/java/it/polimi/ingsw/server/SERVER.java
@@ -2,11 +2,13 @@ package it.polimi.ingsw.server;
 
 import it.polimi.ingsw.server.controller.ServerController;
 
-public class SERVER { public static void main(String[] args) {
-    try {
-        new ServerController();
-    } catch (Exception e) {
-        e.printStackTrace();
+public class SERVER {
+
+    public static void main(String[] args) {
+        try {
+            new ServerController();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
 }
