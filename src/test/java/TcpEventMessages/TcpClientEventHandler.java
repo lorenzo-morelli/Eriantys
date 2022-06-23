bce1df701fdd202a867e45f71197bd1a26a14c3a
fmorea <fernando8morea@gmail.com>
2022-06-23 10:07:22 +0200

    Removed useless network tests

--- src/test/java/TcpEventMessages/TcpClientEventHandler.java
+++
@@ -1,7 +0,0 @@
-package TcpEventMessages;
-
-public interface TcpClientEventHandler{
-    public void onMessage(String line);
-    public void onOpen();
-    public void onClose();
-}
--- src/test/java/TcpEventMessages/TcpServerEventHandler.java
+++
@@ -1,7 +0,0 @@
-package TcpEventMessages;
-
-public interface TcpServerEventHandler{
-    public void onMessage(int client_id, String line);
-    public void onAccept(int client_id);
-    public void onClose(int client_id);
-}
